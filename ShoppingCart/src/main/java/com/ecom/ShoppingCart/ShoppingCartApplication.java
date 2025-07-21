ebd9a191a17fd39efd97f70e1b8f9bea7ff4edd7
NamrataSalve <namratasalve1999@gmailcom>
2025-07-21 18:42:54 +0530

    [Namrata]: tarted witth new project ShoppingCart

---
+++ ShoppingCart/src/main/java/com/ecom/ShoppingCart/ShoppingCartApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.ShoppingCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApplication.class, args);
+		System.out.println("Welcome to Online Shopping Cart project");
+	}
+
+}
